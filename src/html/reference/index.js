6ef4920d97641a78eb8f661b7e10d68ae14e9f57
Cefn Hoile <git@cefn.com>
2015-04-03 14:37:39 +0100

    Included refills' vertical tab js logic, included hashchange plugin and implemented hashhandler to detect navigation internal to page.

--- src/html/reference/index.js
+++ src/html/reference/index.js
@@ -1,12 +1,16 @@
 require.config({
     baseUrl: 'require',
     paths: {
-        jquery: 'jquery-1.11.1'
+        jquery: 'jquery-1.11.1',
+        hashchange: 'jquery.ba-hashchange'
     }
 });
 
 require(['refills/navigation']);
 require(['refills/accordion-tabs']);
+require(['refills/vertical-tabs']);
 require(['refills/scroll']);
 require(['refills/sliding-panel']);
 require(['refills/parallax']);
+
+require(['shrimpingit/hashhandler']);
