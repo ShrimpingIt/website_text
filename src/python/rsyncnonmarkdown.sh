1b11d84ec0d2d7ea6be754c2279b7146655f79a4
Cefn Hoile <git@cefn.com>
2015-04-08 11:24:10 +0100

    Added logic to remove all folders first. Added exclusion array to allow it to be specified in one place

--- src/python/rsyncnonmarkdown.sh
+++ src/python/rsyncnonmarkdown.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-find ../../build/raw/ -type f -not -name '.gitignore' -delete
-find ../../build/styled/ -type f -not -name '.gitignore' -delete
-rsync --verbose --recursive --exclude '*.md' ../../src/content/ ../../build/raw/
-rsync --verbose --recursive --exclude '*.md' ../../src/content/ ../../build/styled/
+EXCLUDE=(
+--exclude="*.md" 
+--exclude="*.scss" 
+--exclude="sass" 
+--exclude=".sass-cache"
+)
+
+find ../../build/raw/ -mindepth 1 -maxdepth 1 -not -name '.gitignore' | tail -n +1 | xargs -n1 rm -rf
+find ../../build/styled/ -mindepth 1 -maxdepth 1 -not -name '.gitignore' | tail -n +1 | xargs -n1 rm -rf
+rsync --verbose --recursive "${EXCLUDE[@]}" ../../src/content/ ../../build/raw/
+rsync --verbose --recursive "${EXCLUDE[@]}" ../../src/content/ ../../build/styled/
