344d7cd074c77acabb387a3efc6813f7451d6831
Cefn Hoile <git@cefn.com>
2015-04-08 13:48:48 +0100

    Added variable for list of rsync exclusions. Added logic to remove folders before syncing. Changed serverroot for live to use full URL.

--- src/python/writelive.sh
+++ src/python/writelive.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-rsync --verbose --recursive --exclude '*.md' ../../src/content/ ../../../shrimpingit.github.io/ 
-/
+EXCLUDE=(
+--exclude="*.md" 
+--exclude="*.scss" 
+--exclude="sass" 
+--exclude=".sass-cache"
+)
+find ../../../shrimpingit.github.io/ -mindepth 1 -maxdepth 1 -not -name 'CNAME' -and -not -name '.git' -and -not -name '.gitignore' | tail -n +1 | xargs -n1 rm -rf
+rsync --verbose --recursive "${EXCLUDE[@]}"  ../../src/content/ ../../../shrimpingit.github.io/ 
 ./writeraw.py
-./writestyled.py --outputdirectory="../../../shrimpingit.github.io/" --serverroot '/'
+./writestyled.py --outputdirectory='../../../shrimpingit.github.io/' --serverroot='http://start.shrimping.it/'
