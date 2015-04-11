53a8934c7457d5c66cac61042a1b5d838fe4ac79
Cefn Hoile <git@cefn.com>
2015-04-11 09:05:30 +0100

    Added explicit sass invocation

--- src/python/writelive.sh
+++ src/python/writelive.sh
@@ -5,6 +5,7 @@ EXCLUDE=(
 --exclude="sass" 
 --exclude=".sass-cache"
 )
+sass ../../src/content/style/sass/index.scss:../../src/content/style/index.css
 find ../../../shrimpingit.github.io/ -mindepth 1 -maxdepth 1 -not -name 'CNAME' -and -not -name '.git' -and -not -name '.gitignore' | tail -n +1 | xargs -n1 rm -rf
 rsync --verbose --recursive "${EXCLUDE[@]}"  ../../src/content/ ../../../shrimpingit.github.io/ 
 ./writeraw.py
